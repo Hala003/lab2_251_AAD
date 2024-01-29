6fb4f4fe9752cf3349c47ed2619881522dd2e3d7
Hala_Alghamdi <hmlghamdi0002@stu.kau.edu.sa>
2024-01-29 11:55:55 +0300

    write a single comm

---
+++ src/lab251/pkg2/Lab2512.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab251.pkg2;
+
+/**
+ *
+ * @author 2108435
+ */
+public class Lab2512 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      System.out.println("HELLO GITHUB WORLD");
+    }
+    
+}
